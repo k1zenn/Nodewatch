1e039a7b555098594ea9be9eca1c7d3fded6f403
k1zenn <purunjay.sh@gmail.com>
2026-03-02 19:22:31 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 
 
